f16e08b23bdba6516d7ffe96a20a36f6201f08d6
behruz171 <behruzzo662@gmail.com>
2023-07-23 22:07:40 +0500

    the background animation has changed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// let text1 = "hello i am front end develop. it's my porfolio"
+// it's speking input ----------------------------
 let p2_text = document.querySelector("h2")
 let timeoutId;
 const debounce = (calback) =>{
@@ -12,17 +12,19 @@ p2_text.oninput = (e)=>{
         speechSynthesis.speak(utterance);
     })
 }
+// -----------------------------------------------
 
 
-
-
+// it's background animation ---------------------------
 let bubbles = document.querySelector('.bubbles')
 for(let i = 1;i<=37;i++){
     let box = document.createElement('span')
     box.style = `--i:${Math.floor(Math.random()*30) + 4};`
     bubbles.appendChild(box)
 }
+//-------------------------------------------------
 
+// it's animation time opacity --------------------------
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
         if(entry.isIntersecting){
@@ -38,3 +40,28 @@ const hiddenElements = document.querySelectorAll('.container');
 hiddenElements.forEach((el)=>observer.observe(el));
 const text = document.querySelectorAll('.select_input');
 text.forEach((el)=>observer.observe(el))
+// -----------------------------------------------------------
+
+// it's animation scrolbar -----------------------------
+const div = document.querySelector(".scrol")
+
+window.addEventListener("scroll",()=>{
+    const {scrollTop,clientHeight,scrollHeight}= document.documentElement;
+    const scrolled = (scrollTop/(scrollHeight-clientHeight)*100)
+    div.style.width = `${scrolled}%`
+})
+
+let wrap = document.querySelector('.wrapper')
+
+wrap.onclick = ()=>{
+    let bub = document.querySelector('.bubbles')
+    bub.classList.toggle('var')
+    nimadir()
+}
+
+function nimadir(){
+    let bub = document.querySelector('.bubbles')
+    debounce(()=>{
+        bub.classList = 'bubbles'
+    })
+}

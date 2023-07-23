29a05c47799f33b20a51d4debbc99e472e3d5490
behruz171 <behruzzo662@gmail.com>
2023-07-23 20:30:52 +0500

    nimadir

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// let text1 = "hello i am front end develop. it's my porfolio"
+let p2_text = document.querySelector("h2")
+let timeoutId;
+const debounce = (calback) =>{
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(()=> calback(),1200)
+}
+
+p2_text.oninput = (e)=>{
+    let utterance = new SpeechSynthesisUtterance(e.target.innerHTML)
+    debounce(()=>{
+        speechSynthesis.speak(utterance);
+    })
+}
+
+
+
+
 let bubbles = document.querySelector('.bubbles')
 for(let i = 1;i<=37;i++){
     let box = document.createElement('span')

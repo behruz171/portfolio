6f23f692eef3eb3d1fa5bb1a48333b5ced5d7842
behruz171 <behruzzo662@gmail.com>
2023-07-23 17:51:32 +0500

    nimadir

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ for(let i = 1;i<=37;i++){
     box.style = `--i:${Math.floor(Math.random()*30) + 4};`
     bubbles.appendChild(box)
 }
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.container');
+hiddenElements.forEach((el)=>observer.observe(el));
+const text = document.querySelectorAll('.select_input');
+text.forEach((el)=>observer.observe(el))

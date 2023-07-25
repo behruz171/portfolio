4a86446713e6a8c63ffba12fca33860c31771cfe
behruz171 <behruzzo662@gmail.com>
2023-07-25 15:57:36 +0500

    nimadir

--- main.js
+++ main.js
@@ -82,3 +82,13 @@ function nimadir(){
 //         console.log(scrolled);
 //     }
 // })
+
+const list = document.querySelectorAll('.list')
+function activeLink(){
+    list.forEach((item)=>
+    item.classList.remove('active'));
+    this.classList.add('active')
+    
+}
+list.forEach((item)=>
+item.addEventListener('click',activeLink));
